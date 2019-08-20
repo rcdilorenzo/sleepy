05d0b96a14068260220c30e0603bccd013a04ca8
Christian Di Lorenzo <rcddeveloper@icloud.com>
2019-08-20 09:25:52 -0400

    Refactor terraform for improved readability

--- deploy/lambda_api_post_event.tf
+++ deploy/lambda_api_post_event.tf
@@ -6,7 +6,56 @@ resource "aws_lambda_function" "post_sleep_event" {
   source_code_hash = "${filebase64sha256("../build/postEvent.zip")}"
 
   runtime = "nodejs10.x"
-  role = "${aws_iam_role.lambda_exec.arn}"
+  role = "${aws_iam_role.post_sleep_event.arn}"
 
-  depends_on    = ["aws_iam_role_policy_attachment.lambda_logs", "aws_cloudwatch_log_group.post_sleep_event"]
+  depends_on    = ["aws_cloudwatch_log_group.post_sleep_event"]
+}
+
+resource "aws_iam_role" "post_sleep_event" {
+  name = "post_sleep_event"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "post_sleep_event" {
+  policy_arn = "${aws_iam_policy.post_sleep_event.arn}"
+  role = "${aws_iam_role.post_sleep_event.name}"
+}
+
+resource "aws_iam_policy" "post_sleep_event" {
+  name = "post_sleep_event"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "${aws_cloudwatch_log_group.post_sleep_event.arn}",
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_cloudwatch_log_group" "post_sleep_event" {
+  name              = "/aws/lambda/PostSleepEvent"
+  retention_in_days = 14
 }
--- deploy/lambda_sqs_handle_job.tf
+++ deploy/lambda_sqs_handle_job.tf
@@ -6,9 +6,9 @@ resource "aws_lambda_function" "handle_job" {
   source_code_hash = "${filebase64sha256("../build/handleJob.zip")}"
 
   runtime = "nodejs10.x"
-  role = "${aws_iam_role.lambda_exec_with_sqs.arn}"
+  role = "${aws_iam_role.handle_job.arn}"
 
-  depends_on    = ["aws_iam_role_policy_attachment.lambda_logs", "aws_cloudwatch_log_group.handle_job"]
+  depends_on    = ["aws_cloudwatch_log_group.handle_job"]
 }
 
 resource "aws_lambda_event_source_mapping" "event_source_mapping" {
@@ -16,4 +16,63 @@ resource "aws_lambda_event_source_mapping" "event_source_mapping" {
   event_source_arn  = "${aws_sqs_queue.job_queue.arn}"
   enabled           = true
   function_name     = "${aws_lambda_function.handle_job.arn}"
+}
+
+resource "aws_iam_role" "handle_job" {
+  name = "handle_job"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "handle_job" {
+  policy_arn = "${aws_iam_policy.handle_job.arn}"
+  role = "${aws_iam_role.handle_job.name}"
+}
+
+resource "aws_iam_policy" "handle_job" {
+  name = "handle_job"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "${aws_cloudwatch_log_group.handle_job.arn}",
+      "Effect": "Allow"
+    },
+    {
+      "Action": [
+        "sqs:ChangeMessageVisibility",
+        "sqs:DeleteMessage",
+        "sqs:GetQueueAttributes",
+        "sqs:ReceiveMessage"
+      ],
+      "Resource": "${aws_sqs_queue.job_queue.arn}",
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_cloudwatch_log_group" "handle_job" {
+  name              = "/aws/lambda/HandleJob"
+  retention_in_days = 14
 }
